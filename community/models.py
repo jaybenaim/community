ca1e9d679dfb27b45c7fb2bb3fb0014781afa549
jaybenaim <benaimjacob@gmail.com>
2019-09-04 22:22:40 -0400

    created Item and UserProfile models

--- community/models.py
+++ community/models.py
@@ -5,3 +5,20 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 from datetime import date 
 from django import forms 
+
+class Item(models.Model): 
+    name_of_item = models.CharField(max_length=255)
+    price = models.CharField(max_length=255)
+    date_lent = models.DateTimeField(auto_now_add=True)
+    date_borrowed = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self): 
+        return self.name_of_item
+
+class UserProfile(models.Model): 
+    # name = models.CharField(max_length=255) get from request 
+    email = models.EmailField(max_length=255) 
+    address = models.CharField(max_length=255)
+    shed_items = models.ForeignKey(Item, on_delete=models.CASCADE) 
+
+
