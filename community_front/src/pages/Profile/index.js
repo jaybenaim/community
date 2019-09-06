ceaad079ee187a7d239399852aef58be6d92dce0
jaybenaim <benaimjacob@gmail.com>
2019-09-06 12:21:45 -0400

    fixed object as child error

--- community_front/src/pages/Profile/index.js
+++ community_front/src/pages/Profile/index.js
@@ -3,32 +3,36 @@ import React, { Component } from "react";
 import axios from "axios";
 
 class Profile extends Component {
-  //   const [items, setItems] = useState({});
-  //   const [profiles, setProfile] = useState({});
   state = {
     items: []
   };
-  //   useEffect(() => {
   componentDidMount() {
     axios
       .get("http://localhost:8000/api")
-      //   .then(res => res.json())
+
       .then(res => {
         let items = res.data.items;
-        for (let i = 0; i < items.length; i++) {
-          this.setState({ items: [...this.state.items, items[i].name] });
-        }
-        // setItems(res.data.items);
-        // setProfile(res.profiles);
-        // console.log(res.data.items);
-        // console.log(items);
+
+        items.map(item => {
+          this.setState({
+            items: [{ name: item.name, price: item.price }]
+          });
+        });
       });
-    //   });
   }
   render() {
     return (
       <>
-        <h1> {this.state.items}</h1>
+        <ul>
+          {this.state.items.map((item, index) => {
+            return (
+              <li key={index}>
+                Item: {item.name} || Price: {item.price}
+              </li>
+            );
+          })}
+          {/* <li>{this.state.items.name}</li> */}
+        </ul>
         {/* <h1>Items{JSON.stringify(items)}</h1>
       <h2>Profiles{JSON.stringify(profiles)}</h2> */}
       </>
