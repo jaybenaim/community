57c7b320de5f0d9486eb82d91bae6d200a188a50
jaybenaim <benaimjacob@gmail.com>
2019-09-05 23:17:49 -0400

    navbar template done and logo added

--- community_front/src/App.js
+++ community_front/src/App.js
@@ -1,28 +1,20 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import Button from "react-bootstrap/Button";
+import { Helmet } from "react-helmet";
+import NavBar from "./components/Navbar";
 
 const App = () => {
   return (
     <Router>
-      <head>
-        <script
-          src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"
-          integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o"
-          crossorigin="anonymous"
-        ></script>
-      </head>
-      <div className="container">
-        <div className="App">
-          <nav className="nav navbar">
-            <Link to="/">Home</Link>
-          </nav>
-          <Switch>
-            <Route></Route>
-          </Switch> 
-        </div>
+      <div className="App">
+        <Switch>
+          <NavBar />
+          {/* <Route
+            path="/procedures"
+            render={props => <Procedures {...props} />} */}
+          />
+        </Switch>
       </div>
     </Router>
   );
---
+++ community_front/src/components/Navbar/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import { Helmet } from "react-helmet";
+import logo from "../../logo/logo.png";
+import styles from "./index.css";
+const NavBar = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="/home">
+        <img className="logo" src={logo} alt="logo" />
+        Community
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/home">Home</Nav.Link>
+          <Nav.Link href="/profiles">Profiles</Nav.Link>
+          <Nav.Link href="/map">Map</Nav.Link>
+          <NavDropdown title="Category" id="basic-nav-dropdown">
+            <NavDropdown.Item href="/category/outdoor">
+              Outdoor
+            </NavDropdown.Item>
+            <NavDropdown.Item href="/category/indoor">Indoor</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="/category/powertools">
+              Power Tools
+            </NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="/category/saws">Saws</NavDropdown.Item>
+            <NavDropdown.Item href="/category/ladders">
+              Ladders
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default NavBar;
