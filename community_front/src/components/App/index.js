1369f84e75f3232f4d2eedf741a0140eb43f2618
jaybenaim <benaimjacob@gmail.com>
2019-09-12 22:00:06 -0400

    merge fix

--- community_front/src/components/App/index.js
+++ community_front/src/components/App/index.js
@@ -8,7 +8,6 @@ import SimpleMap from "../Map";
 import AllProfiles from "../AllProfiles";
 import Root from "../../apis/root";
 
-<<<<<<< HEAD
 class App extends React.Component {
   state = {
     profileName: "",
@@ -23,44 +22,17 @@ class App extends React.Component {
   };
   handleAddItemName = event => {
     let itemName = event.target.value;
-    // setItemName({ itemName: itemName });
     this.setState({ itemName: itemName });
   };
   handleAddItemPrice = event => {
     let itemPrice = event.target.value;
-    // setItemPrice({ itemPrice: itemPrice });
     this.setState({ itemPrice: itemPrice });
-=======
-const App = () => {
-  // Hooks
-
-  const [profileName, setProfileName] = useState("");
-  const [email, setEmail] = useState("");
-  const [address, setAddress] = useState("");
-  const [show, setShow] = useState(false);
-  const [showProfile, setProfile] = useState(false);
-  const [displayItemForm, setDisplayItemForm] = useState(false);
-
-  const [itemName, setItemName] = useState("");
-  const [itemPrice, setItemPrice] = useState("");
-
-  const onChangeItemName = newName => {
-    setItemName(newName);
-  };
-  const onChangeItemPrice = newPrice => {
-    setItemPrice(newPrice);
->>>>>>> 65347de3ab93e4bc5e2c4e5d134bb8c94fd0bb1b
   };
 
   handleFormSubmit = () => {
     Root.post("items/", {
-<<<<<<< HEAD
       name_of_item: this.state.itemName.itemName,
       price: this.state.itemPrice.itemPrice
-=======
-      name_of_item: itemName,
-      price: itemPrice
->>>>>>> 65347de3ab93e4bc5e2c4e5d134bb8c94fd0bb1b
     })
       .then(res => {
         setDisplayItemForm(false);
@@ -77,15 +49,7 @@ const App = () => {
     this.setState(prevState => !prevState);
   };
 
-<<<<<<< HEAD
   handleProfileFormSubmit = values => {
-=======
-  const handleItemClose = () => {
-    setDisplayItemForm(false);
-  };
-
-  const handleProfileFormSubmit = values => {
->>>>>>> 65347de3ab93e4bc5e2c4e5d134bb8c94fd0bb1b
     let name = values.profile_name;
     let email = values.email;
     let address = values.address;
@@ -111,7 +75,6 @@ const App = () => {
   getAllItems = () => {
     return null;
   };
-<<<<<<< HEAD
   componentDidMount() {
     this.getAllProfiles();
   }
@@ -170,57 +133,5 @@ const App = () => {
     );
   }
 }
-=======
-  const handleShow = () => setShow(true);
-
-  return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={Home} />
-        </Switch>
-        <Switch>
-          <NavBar />
-        </Switch>
-        <Switch>
-          <Route
-            exact
-            path="/profiles"
-            render={props => (
-              <Profile
-                profileName={profileName}
-                email={email}
-                address={address}
-                show={show}
-                displayItemForm={displayItemForm}
-                showProfile={showProfile}
-                itemName={itemName}
-                itemPrice={itemPrice}
-                displayItemForm={displayItemForm}
-                handleShow={handleShow}
-                handleClose={handleClose}
-                handleItemClose={handleItemClose}
-                onChangeItemName={onChangeItemName}
-                handleFormSubmit={handleFormSubmit}
-                onChangeItemPrice={onChangeItemPrice}
-                handleAddItemToggle={handleAddItemToggle}
-                handleShowProfile={handleShowProfile}
-                handleProfileFormSubmit={handleProfileFormSubmit}
-                handleFormSubmit={handleFormSubmit}
-              />
-            )}
-          />
-        </Switch>
-        <Switch>
-          <Route path="/all" component={Profiles} />
-        </Switch>
-        <Switch>
-          <Route path="/map" component={SimpleMap} />
-        </Switch>
-      </div>
-    </Router>
-  );
-};
->>>>>>> 65347de3ab93e4bc5e2c4e5d134bb8c94fd0bb1b
 
 export default App;
