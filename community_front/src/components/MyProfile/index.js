cd6ac240ad5fb8aae562c34675264378e707134f
jaybenaim <benaimjacob@gmail.com>
2019-09-14 13:38:37 -0400

    something went wrong

--- community_front/src/components/MyProfile/index.js
+++ community_front/src/components/MyProfile/index.js
@@ -4,17 +4,48 @@ import Root from "../../apis/root";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import ImageApi from "../../apis/images";
+import IMAGE_ACCESS_KEY from "../../apis/keys";
+import PEXELS_API_KEY from "../../apis/keys";
+import GIPHY_API_KEY from "../../apis/keys";
+import ProfileItem from "../ProfileItem";
+
+import Axios from "axios";
+
 class MyProfile extends React.Component {
   state = {
-    items: []
+    items: [],
+    image: "",
+    query: ""
   };
   getToken = () => {
-    Root.get("profiles/").then(res => {
+    Root.get(`token-auth/`).then(res => {
       const { data } = res;
       // data.map();
-      console.log(res.user);
+      // console.log(res.user);
     });
   };
+  getImages = async () => {
+    Root.get("items/").then(res => {
+      const { name_of_item, price } = res.data[0];
+      console.log(name_of_item);
+      this.setState({ query: name_of_item });
+    });
+    await Axios.get(
+      `https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=${this.name_of_item}&limit=1&offset=0&rating=G&lang=en`
+    )
+      .then(res => {
+        console.log(res.data.data[0].images.fixed_height_still);
+        const { url } = res.data.data[0].images.fixed_height_still;
+        this.setState({
+          image: url
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   getProfileName = () => {
     const { allProfiles } = this.props;
     allProfiles.map(profile => {
@@ -23,8 +54,10 @@ class MyProfile extends React.Component {
       return profile;
     });
   };
+
   render() {
-    this.getToken();
+    // this.getToken();
+    // this.getImages();
     // this.getProfileName();
 
     return (
@@ -35,6 +68,7 @@ class MyProfile extends React.Component {
               className="profile-image"
               src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
               alt="profile"
+              onClick={this.getImages}
             />
             <Container>
               <Row>
@@ -42,7 +76,7 @@ class MyProfile extends React.Component {
               </Row>
             </Container>
           </Col>
-          <Col className="myprofile-container">Item Image</Col>
+          <ProfileItem />
           <Col className="myprofile-container">Item Details</Col>
         </Row>
       </Container>
---
+++ community_front/src/components/ProfileItem/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Col from "react-bootstrap/Col";
+
+class ProfileItem extends React.Component {
+  state = {};
+  render() {
+    return (
+      <Col className="myprofile-container">
+        Item Image
+        <ProfileItem />
+        <img src={this.state.image} alt="item" />
+      </Col>
+    );
+  }
+}
+
+export default ProfileItem;
