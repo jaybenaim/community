728e09e33c8326944add5b1c9f2a18378f0cb431
jaybenaim <benaimjacob@gmail.com>
2019-09-15 15:49:08 -0400

    added udpate method and tested now going to move into the profile page

--- community_front/src/components/PutTest/index.js
+++ community_front/src/components/PutTest/index.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
+import Root from "../../apis/root";
+import "./index.css";
 
 class PutTest extends Component {
   state = {
-    item: []
+    item: [],
+    profile_id: "",
+    name_of_item: "",
+    price: ""
   };
-  updateItem = () => {};
+  // todo make form to set state
+  // send values of set state to post
+  updateItem = () => {
+    Root.put("items/2/", {
+      profile_id: 14,
+      name_of_item: "ladder",
+      price: "mow the lawn"
+    }).then(res => {
+      console.log(res);
+      let name_of_item = "green eggs and ham";
+      let price = "mow the lawn";
+      //   const { name_of_item, price } = this.state.item;
+      //   this.setState({ item: { [name_of_item]: price } });
+      this.setState({ item: { [name_of_item]: price } });
+      console.log(this.state.item);
+    });
+  };
   render() {
     return (
-      <button type="button" onClick={this.updateItem}>
+      <button className="test" type="button" onClick={this.updateItem}>
         {" "}
         Update Item{" "}
       </button>
