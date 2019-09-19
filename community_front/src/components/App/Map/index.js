044650096d9796cbf7ced8b1034caf437cd59709
jaybenaim <benaimjacob@gmail.com>
2019-09-19 03:56:43 -0400

    map broke

--- community_front/src/components/App/Map/index.js
+++ community_front/src/components/App/Map/index.js
@@ -4,6 +4,7 @@ import GoogleMapReact from "google-map-react";
 import { MAP_API_KEY } from "../../../apis/keys";
 import Geocode from "react-geocode";
 import MapMarker from "./MapMarker";
+import Root from "../../../apis/root";
 
 /**
  *@prop [allProfiles] array required
@@ -16,20 +17,14 @@ class Map extends Component {
       geocodes: [],
       center: { lat: 43.99, lng: -79 },
       zoom: 10,
-      loading: false
+      loading: false,
+      users: []
     };
   }
 
   componentDidMount() {
     // const { allProfiles } = this.props;
-    this.initializeGeocode();
-
-    // when we load this component, we neeed to populate geocode state object
-    // so that the markers are ready to render
-    // allProfiles.forEach(({ address }, index) => {
-    //   this.getGeocodeFromAddress(address);
-    //   this.checkGeocodeLoading(index);
-    // });
+    this.initializeGeocode(); // when we load this component, we neeed to populate geocode state object // so that the markers are ready to render // allProfiles.forEach(({ address }, index) => { //   this.getGeocodeFromAddress(address); //   this.checkGeocodeLoading(index); // });
   }
 
   componentDidUpdate(prevProps, prevState) {
@@ -43,6 +38,11 @@ class Map extends Component {
       });
     }
   }
+  getProfileAddresses = () => {
+    Root.get("profiles/").then(res => {
+      this.setState({ users: res.data });
+    });
+  };
 
   checkGeocodeLoading = index => {
     const { allProfiles } = this.props;
@@ -59,33 +59,26 @@ class Map extends Component {
     const { setApiKey, enableDebug } = Geocode;
     setApiKey(MAP_API_KEY);
     enableDebug();
-  };
+  }; // MAKE THE MARKERS !!!!
 
-  // MAKE THE MARKERS !!!!
   covertProfileMarkers = () => {
-    const { allProfiles } = this.props;
-    // removes the numbers from the address
+    const { allProfiles } = this.props; // removes the numbers from the address
     allProfiles.map(({ address }) =>
       address.substring(address.indexOf(" "), address.length)
     );
-  };
+  }; // Get Geocodes For Markers
 
-  // Get Geocodes For Markers
   getGeocodeFromAddress = address => {
     Geocode.fromAddress(address.toString()).then(res => {
       // grab the lat lng values from the geocode api
-      const { lat, lng } = res.results[0].geometry.location;
+      const { lat, lng } = res.results[0].geometry.location; // grab the current list of geocodes
 
-      // grab the current list of geocodes
-      const { geocodes } = this.state;
+      const { geocodes } = this.state; // add the geocodes to my exisitng list
 
-      // add the geocodes to my exisitng list
       this.setState({ geocodes: [...geocodes, { lat, lng }] });
     });
   };
-  componentDidMount = () => {
-    this.props.handleNavClassChange();
-  };
+
   render() {
     // Setup the Render variables
     const { allProfiles } = this.props;
@@ -94,20 +87,23 @@ class Map extends Component {
     if (!loading) {
       return (
         <section style={{ height: "100vh", width: "100%" }}>
+                    
           <GoogleMapReact
             bootstrapURLKeys={{ key: MAP_API_KEY }}
             defaultCenter={center}
             defaultZoom={zoom}
             yesIWantToUseGoogleMapApiInternals
           >
+                        
             {allProfiles.map((profile, index) => {
-              const { id } = profile;
-              // Grab the lat, lng from the object inside the geocode array from state
+              const { id } = profile; // Grab the lat, lng from the object inside the geocode array from state
               const { lat, lng } = geocodes[index] || {};
 
               return <MapMarker key={id} {...profile} lat={lat} lng={lng} />;
             })}
+                      
           </GoogleMapReact>
+                  
         </section>
       );
     }
